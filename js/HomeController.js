7712e560a8ef4572af12084b783699f200000563
hassanefall <hfall@outlook.com>
2016-05-26 14:49:58 -0400

    update style sheet

--- js/HomeController.js
+++ js/HomeController.js
@@ -8,14 +8,14 @@ bestService.success(function(data){
     $scope.bestSearch= data;
 });
 $scope.submit= function () {
+    var valid= document.getElementById('check');
     if($scope.books === ''){
-        alert('Field is empty');
+        valid.innerHTML="<p class='alert alert-danger'>Oops! The field is empty!</p>";
         return false;
     } 
     else {
         // If search field is empty, render 'no'. Otherwise change to 'Your' when search is not empty 
-        var valid= document.getElementById('check');
-        valid.innerHTML= 'Your';
+        valid.innerHTML= 'Results for: '+ $scope.books;
        console.log($scope.books); 
        bookSearch.getBooks($scope.books).success(function(data){
             $scope.searchResult = data;
