2c5467b9f761147d67da30f0b9cce5ab6b474f70
hassanefall <hfall@outlook.com>
2016-05-28 22:33:06 -0400

    add loader image

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 var app = angular.module('coolMark', ['ngRoute']);
 
+// ADDING LOADING GRAPHIC FOR SEARCH USING CUSTOM DIRECTIVE
+app.directive('loading', ['$http', function($http){
+	return {
+		restrict: 'A',
+		link:function(scope, element, attrs){
+			scope.isLoading = function(){
+				return $http.pendingRequests.length > 0;
+			};
+			scope.$watch(scope.isLoading, function(value) {
+				if(value) {
+					element.removeClass('ng-hide');
+				} else {
+					element.addClass('ng-hide');
+				}
+			});
+		}
+	};
+}]);
+
 app.config(function($routeProvider){
 	$routeProvider
 	.when('/', {
